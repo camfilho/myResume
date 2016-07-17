7c17d9049ec6861910239d7bccc4417b40782690
carlosfilhomedeiros <carlos.au.filho@gmail.com>
2016-07-17 10:48:12 -0300

    add page not found in english

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from app import meuapp
+flag = False
 
 
 @meuapp.route('/')
 def index():
+    global flag
+    flag = False
     return render_template("index_pt.html")
 
 
@@ -14,6 +17,9 @@ def index_portuguese():
 
 @meuapp.route('/en')
 def index_english():
+    global flag
+    flag = True
+    print(flag)
     return render_template("index.html")
 
 
@@ -24,4 +30,8 @@ def ranQuotes():
 
 @meuapp.errorhandler(404)
 def page_not_found(e):
-    return render_template("underconstruction.html"), 404
+    print(flag)
+    if (not(flag)):
+        return render_template("underconstruction.html"), 404
+    else:
+        return render_template("underconstruction_en.html"), 404
