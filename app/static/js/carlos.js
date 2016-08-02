37368ddc34c34a92e1ac546c7c808c2a32de01d9
carlosfilhomedeiros <carlos.au.filho@gmail.com>
2016-08-02 18:53:55 -0300

    changed overall structre, link section with the nav header

--- app/static/js/carlos.js
+++ app/static/js/carlos.js
@@ -1,22 +1,36 @@
 
           $(document).ready(function(){
-           // Activate the side menu
+            console.log('ready');
+            //Activate Smooth Scroll
+            var $root = $('html, body');
+            $('.smooth a').click(function() {
+                $root.animate({
+                    scrollTop: $( $.attr(this, 'href') ).offset().top
+                }, 500);
+                return false;
+            })
+            //Mobile Sidebar
             $(".button-collapse").sideNav();
-          });
- 
-          window.twttr = (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0],
-            t = window.twttr || {};
-            if (d.getElementById(id)) return t;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
 
-            t._e = [];
-            t.ready = function(f) {
-              t._e.push(f);
-           };
 
-           return t;
-          }(document, "script", "twitter-wjs"));
+
+            //Twitter Functions
+            window.twttr = (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0],
+              t = window.twttr || {};
+              if (d.getElementById(id)) return t;
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https://platform.twitter.com/widgets.js";
+              fjs.parentNode.insertBefore(js, fjs);
+
+              t._e = [];
+              t.ready = function(f) {
+                t._e.push(f);
+             };
+
+             return t;
+            }(document, "script", "twitter-wjs"))
+
+
+        })
